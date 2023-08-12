b78f05ee0007856db941e289b33a79fd4d342499
alireza4585 <alimohammadan84@gmail.com>
2023-08-12 15:19:03 +0330

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_to_do_list/auth/auth_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
