01da684599a04d1591c50915a3ceb10a88d2f453
alireza4585 <alimohammadan84@gmail.com>
2023-08-19 15:18:38 +0330

    create task widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_to_do_list/auth/main_page.dart';
+import 'package:flutter_to_do_list/screen/home.dart';
+import 'package:flutter_to_do_list/widgets/task_widgets.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Main_Page(),
+      home: Home_Screen(),
     );
   }
 }
---
+++ lib/widgets/task_widgets.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_to_do_list/const/colors.dart';
+
+class Task_Widget extends StatefulWidget {
+  const Task_Widget({super.key});
+
+  @override
+  State<Task_Widget> createState() => _Task_WidgetState();
+}
+
+bool isDone = false;
+
+class _Task_WidgetState extends State<Task_Widget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColors,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          child: Container(
+            width: double.infinity,
+            height: 130,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.2),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 2),
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  // image
+                  imageee(),
+                  SizedBox(width: 25),
+                  // title and subtitle
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 5),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'title',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Checkbox(
+                              activeColor: custom_green,
+                              value: isDone,
+                              onChanged: (value) {
+                                setState(() {
+                                  isDone = !isDone;
+                                });
+                              },
+                            )
+                          ],
+                        ),
+                        Text(
+                          'subtitle',
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.grey.shade400),
+                        ),
+                        Spacer(),
+                        edit_time()
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget edit_time() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        children: [
+          Container(
+            width: 90,
+            height: 28,
+            decoration: BoxDecoration(
+              color: custom_green,
+              borderRadius: BorderRadius.circular(18),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 6,
+              ),
+              child: Row(
+                children: [
+                  Image.asset('images/icon_time.png'),
+                  SizedBox(width: 10),
+                  Text(
+                    'time',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(width: 20),
+          Container(
+            width: 90,
+            height: 28,
+            decoration: BoxDecoration(
+              color: Color(0xffE2F6F1),
+              borderRadius: BorderRadius.circular(18),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 6,
+              ),
+              child: Row(
+                children: [
+                  Image.asset('images/icon_edit.png'),
+                  SizedBox(width: 10),
+                  Text(
+                    'edit',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget imageee() {
+    return Container(
+      height: 130,
+      width: 100,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        image: DecorationImage(
+          image: AssetImage('images/1.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
