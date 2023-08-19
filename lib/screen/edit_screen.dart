35f673dafaf6ddf7e8e0e90b52bee821b26a5688
alireza4585 <alimohammadan84@gmail.com>
2023-08-19 17:09:51 +0330

    add edit screen

---
+++ lib/screen/edit_screen.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_to_do_list/const/colors.dart';
+
+class Edit_Screen extends StatefulWidget {
+  const Edit_Screen({super.key});
+
+  @override
+  State<Edit_Screen> createState() => _Edit_ScreenState();
+}
+
+class _Edit_ScreenState extends State<Edit_Screen> {
+  final title = TextEditingController();
+  final subtitle = TextEditingController();
+
+  FocusNode _focusNode1 = FocusNode();
+  FocusNode _focusNode2 = FocusNode();
+  int indexx = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColors,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            title_widgets(),
+            SizedBox(height: 20),
+            subtite_wedgite(),
+            SizedBox(height: 20),
+            imagess(),
+            SizedBox(height: 20),
+            button()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget button() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: custom_green,
+            minimumSize: Size(170, 48),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('add task'),
+        ),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.red,
+            minimumSize: Size(170, 48),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Cancel'),
+        ),
+      ],
+    );
+  }
+
+  Container imagess() {
+    return Container(
+      height: 180,
+      child: ListView.builder(
+        itemCount: 4,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                indexx = index;
+              });
+            },
+            child: Padding(
+              padding: EdgeInsets.only(left: index == 0 ? 7 : 0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    width: 2,
+                    color: indexx == index ? custom_green : Colors.grey,
+                  ),
+                ),
+                width: 140,
+                margin: EdgeInsets.all(8),
+                child: Column(
+                  children: [
+                    Image.asset('images/${index}.png'),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget title_widgets() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: TextField(
+          controller: title,
+          focusNode: _focusNode1,
+          style: TextStyle(fontSize: 18, color: Colors.black),
+          decoration: InputDecoration(
+              contentPadding:
+                  EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+              hintText: 'title',
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(
+                  color: Color(0xffc5c5c5),
+                  width: 2.0,
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(
+                  color: custom_green,
+                  width: 2.0,
+                ),
+              )),
+        ),
+      ),
+    );
+  }
+
+  Padding subtite_wedgite() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: TextField(
+          maxLines: 3,
+          controller: subtitle,
+          focusNode: _focusNode2,
+          style: TextStyle(fontSize: 18, color: Colors.black),
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            hintText: 'subtitle',
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                color: Color(0xffc5c5c5),
+                width: 2.0,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                color: custom_green,
+                width: 2.0,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
