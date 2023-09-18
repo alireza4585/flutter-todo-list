b56768dbd8e237bf1b4cfb8b20c844abd2512ea5
Alireza Mohammadian <alirezamohammadian@Alirezas-Mac-mini.local>
2023-09-18 13:08:37 -0700

    fix firebase for ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_to_do_list/auth/main_page.dart';
+import 'package:flutter_to_do_list/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
